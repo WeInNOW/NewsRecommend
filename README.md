ec810fb1b1533fa1065d66590cb77ac2015867d9
lib9017 <lib9017@bupt.edu.cn>
2018-05-30 22:48:35 +0800

    添加自定义词袋

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # NewsRecommend
 first setting
+必然是有改进的，去噪自编码有些改进；
+LSTM - based model也是有改进
+文章维度、以及用户表示维度 500
+中间层维度： 20
+学习率： 0.005
--- embedding-based/deal_text.py
+++ embedding-based/deal_text.py
@@ -13,4 +13,28 @@ def getWordFromNews():
             string = re.sub("[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）]+", " ",dic.get('text'))
             f.write(string)
             f.write('\n')
+def getVector():
+    distinctWord=set()
+    with open('../news/newsTxt.txt') as f:
+        for eachline in f:
+            if len(eachline) < 5:
+                continue
+            distinctWord=distinctWord.union(set(eachline.split()))
+            wordBag(eachline)
+    print(len(distinctWord))
+
 # 计算文章向量- 先只使用词袋表示：
+def wordBag(strs):
+    words=[]  # 存的词，
+    vector=[] # 向量
+    bags=strs.split()
+    for word in bags:
+        if word not in words:
+
+            words.append(word)
+            vector.append(1)
+        else:
+            vector[words.index(word)] += 1
+    return vector
+if __name__ == '__main__':
+    getVector()
